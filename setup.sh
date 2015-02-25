da5d8674eefd1c54d84c05fd9888769591a24312
Florian Schweikert <kelvan@ist-total.org>
2015-02-25 18:39:20 +0100

    Add .Xmodmap

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ git submodule init
 git submodule update --recursive
 
 mkdir ~/.zsh
-wget -O ~/.zsh/zshrc_grml        http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+wget -O ~/.zsh/zshrc_grml http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 touch ~/.zsh/zshrc_personal
 ln -s $ABSOLUTE_PATH/.zshrc ~/.zshrc
 ln -s $ABSOLUTE_PATH/.vimrc ~/.vimrc
 ln -s $ABSOLUTE_PATH/.vim ~/.vim
+ln -s $ABSOLUTE_PATH/.Xmodmap ~/.Xmodmap
